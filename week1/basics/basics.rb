1b0ca1fcd31462a6217ea6ba5f894c69211ad1ee
Kristine <kteich88@gmail.com>
2018-09-18 09:15:41 -0400

    initial commit

---
+++ week1/basics/basics.rb
@@ -0,0 +1,22 @@
+# answer each of the following to the best of your ability
+# there is always more than one way to code a response 😄
+# when you are done, share your code with your instructor, push it to Github, or submit a pull request
+
+
+# output an integer
+
+
+
+# output the number of seconds in a week
+
+
+
+# output a float
+
+
+
+# output a string
+
+
+
+# output concatination of strings
