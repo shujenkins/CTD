0332daed7b229cc4522a3f57070c7fca6e3c844b
Kristine Teichmann <kteich88@gmail.com>
2018-09-18 14:22:34 -0400

    updated basics.rb
    added variable assignments to be included in each answer

--- week1/basics/basics.rb
+++ week1/basics/basics.rb
@@ -1,5 +1,6 @@
 # answer each of the following to the best of your ability
 # there is always more than one way to code a response 😄
+# please use your knowledge of variable assignments in each response
 # when you are done, share your code with your instructor, push it to Github, or submit a pull request
 
 
