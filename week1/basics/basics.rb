eb22e766fe2a619453ac0577f7ce53111c4083e3
Shu Jenkins <shujenkins@gmail.com>
2018-09-20 07:26:08 -0400

    Revert "Update changes to basics.rb"
    This reverts commit 36c0f4fe49e9586d4c003741f37031c5aadb055a.

--- week1/basics/basics.rb
+++ week1/basics/basics.rb
@@ -6,28 +6,18 @@
 
 # output an integer
 
-myNum = 33
-print 'The number ' + myNum.to_s + ' is an integer!'
-puts ''
-puts 'You can output an integer simply by stating below:'
-puts 33
+
 
 # output the number of seconds in a week
 
-secVar = 60 * 60 * 24 * 7
-puts 'There are ' + secVar.to_s + ' seconds in a week!'
+
 
 # output a float
 
-myNum2 = 33.5
-puts 'The number ' + myNum2.to_s + ' is a float or floating-point number.'
-# An output of a float can be shown with multiplication below
-puts 2.0 * 3.5
+
 
 # output a string
 
-puts 'Hello, my name is Shu!'
+
 
 # output concatination of strings
-
-puts "Taco" + "Bell"
